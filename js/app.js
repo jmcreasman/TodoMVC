60d6cc3e3dbf1d05184977bd234fdc5fc76385c7
Jonathan Creasman <jmcreasman89@gmail.com>
2015-09-14 16:51:26 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-
+// User Stories
+// I can add a task to my todo list so that I can remember to do it later.
+  //App will need to collect and store data into the todo list
+// I can edit a task on my todo list so that I can correct typos.
+  //That data will need to be editable by the user
+// I can mark a task as complete so that I remember that I have done it.
+  //Completed tasks will need to "disappear" from the users visibility but the value still needs to be stored
+// I can mark a completed task as incomplete so that I actually complete it this time.
+  //Almost the reverse of the last note. Completed tasks will need to be able to be filtered to they can be "unchecked"
+  //by the user and appear back on the todo list.
+// I can delete a task from my todo list so that I don't have to see it any more.
+  //The todo list item will need to be removed perminently.
+// I can delete all the completed tasks from my todo list so that I can clean up completed tasks.
+  //Same functionality as the above note only these will have been checked.
+// I can filter the list of tasks so that I can see just what tasks I have completed and what are left to do.
+  //Need a database system to file all these tasks into complete and incomplete lists.
